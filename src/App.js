15aeb90ff909a1c6b36d8fcf0637c7e0b2c5c852
RicardoMarques21 <1241923@isep.ipp.pt>
2025-05-19 08:35:09 +0100

    Add a preloader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import './App.css';
 import TechStack from "./pages/TechStack";
-
+import Preloader from './components/Preloader';
+import './App.css';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 3000); // 3 segundos
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <Preloader />; // Mostra preloader antes de tudo
+    }
+
     return (
         <Router>
             <Navbar />
---
+++ src/components/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaDocker } from 'react-icons/fa';
+import './Preloader.css';
+
+export default function Preloader() {
+    return (
+        <div className="preloader">
+            <FaDocker className="preloader-icon" />
+            <p>Loading DevOps Portfolio...</p>
+        </div>
+    );
+}
