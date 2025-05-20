7cd829381ee179b513e9c1f0362b1602b37306ee
RicardoMarques21 <1241923@isep.ipp.pt>
2025-05-20 09:27:08 +0100

    Added hamburguer to navbar on mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'
+import '../styles/Navbar.css';
 
-export default function Navbar() {
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
     return (
         <nav>
             <h1>Ricardo Marques</h1>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/techstack">TechStack</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+
+
+            <div className="hamburger" onClick={toggleMenu}>
+                <span className={isOpen ? 'bar open' : 'bar'}></span>
+                <span className={isOpen ? 'bar open' : 'bar'}></span>
+                <span className={isOpen ? 'bar open' : 'bar'}></span>
+            </div>
+
+
+            <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
+                <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+                <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
+                <li><Link to="/techstack" onClick={() => setIsOpen(false)}>TechStack</Link></li>
+                <li><Link to="/projects" onClick={() => setIsOpen(false)}>Projects</Link></li>
+                <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
             </ul>
         </nav>
     );
 }
+
+export default Navbar;
